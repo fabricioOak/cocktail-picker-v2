2a353c33c1b0ab793298dab85388e394352364b5
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-05-20 01:23:09 -0300

    add the search route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import SearchPage from '@/views/SearchPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/search/:search',
+      name: 'search',
+      component: SearchPage
     },
   ]
 })
