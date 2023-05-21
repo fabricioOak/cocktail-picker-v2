fe5687dd889acba484a71400d37a9c3438a6a928
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-05-21 18:42:05 -0300

    footer component at app level

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MainHeader from './components/MainHeader.vue';
+import MainFooter from './components/MainFooter.vue';
 </script>
 
 <template>
   <MainHeader />
   <RouterView />
+  <MainFooter />
 </template>
