26aafb28bf4070c8f0474ceb31778e1ce31ce2ad
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-11-09 00:08:34 -0300

    make spinning a component

--- src/components/DrinkThumb.vue
+++ src/components/DrinkThumb.vue
@@ -1,16 +1,7 @@
 <template>
-  <div class="flex flex-col items-center justify-center col-span-1">
-    <div class="relative p-5">
-      <div
-        class="absolute z-10 w-full h-full -mt-5 opacity-30 -ml-5 rounded-full rounded-tr-none"
-        :style="{ backgroundColor: getRandomThumbColor() }"
-      ></div>
-      <img
-        :alt="name"
-        @click="emit('open-details')"
-        class="cursor-pointer relative hover:scale-100 scale-95 duration-150 transform z-20 rounded-full"
-        :src="src"
-      />
+  <div class="flex flex-col items-center justify-center sm:w-5/12">
+    <div class="w-3/4 sm:w-1/2">
+      <DrinkImage :name="name" :src="src" @open-details="emit('open-details')" />
     </div>
     <div class="mt-3 space-y-2 text-center">
       <div
@@ -25,6 +16,8 @@
 </template>
 
 <script setup lang="ts">
+import DrinkImage from './DrinkImage.vue'
+
 interface Props {
   src: string
   name: string
@@ -32,17 +25,6 @@ interface Props {
 
 defineProps<Props>()
 const emit = defineEmits(['open-details'])
-
-const getRandomThumbColor = () => {
-  const letters: string = '0123456789ABCDEF'
-  let color: string = '#'
-
-  for (let i: number = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-
-  return color
-}
 </script>
 
 <style scoped>
---
+++ src/components/SpinningAnimation.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-96 w-screen flex items-center justify-center text-center">
+    <div
+      class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-gray-100"
+    ></div>
+  </div>
+</template>
