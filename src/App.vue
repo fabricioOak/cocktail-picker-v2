79cd00eb6580a1afdc392bb03765c4412bc79526
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-11-09 00:07:58 -0300

    add dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import MainHeader from './components/MainHeader.vue';
-import MainFooter from './components/MainFooter.vue';
+import MainHeader from './components/MainHeader.vue'
+// import MainFooter from './components/MainFooter.vue'
 </script>
 
 <template>
-  <MainHeader />
-  <RouterView />
-  <MainFooter />
+  <div class="flex flex-grow flex-col">
+    <MainHeader />
+    <RouterView />
+    <!-- <MainFooter /> -->
+  </div>
 </template>
+
+<style>
+body {
+  @apply dark:bg-slate-800 dark:text-gray-200;
+}
+</style>
